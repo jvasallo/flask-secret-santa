4b7544a8dd483b08a7c6c01cd8712fdb5fb191b8
Joel Vasallo <joelvasallo@gmail.com>
2015-11-26 08:45:15 -0600

    Updated form submission handler

--- santa.py
+++ santa.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+MAX_GIFT_LIMIT = "$50.00 USD"
+
 @app.route('/santa', methods=['POST', 'GET'])
 def santa():
     error = None
     if request.method == 'POST':
-        return 'You Posted!'
+        real_name = request.form.get('name')
+        email_address = request.form.get('email_address')
+	return "You got: %s\nTheir email is: %s\nRemember the max limit of: %s!" % (real_name, email_address, MAX_GIFT_LIMIT)
     return render_template('santa.html', error=error)
 
 if __name__ == '__main__':
