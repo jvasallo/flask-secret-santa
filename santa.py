f95d541bcb60163d6e5381c194c8acf2452b90ae
Joel Vasallo <joelvasallo@gmail.com>
2015-11-26 19:29:22 -0600

    Added debug printing of random numbers

--- santa.py
+++ santa.py
@@ -70,9 +70,9 @@ def randomizer():
                         random_match = random.choice(user_list)
 			if random_match != each_user[0]:
 			    query = "UPDATE secret_santa SET match=%d WHERE name='%s' AND id=%d" % (int(random_match), each_user[1], int(each_user[0]))
-			    print query
 			    g.db.execute(query)
 			    g.db.commit()
+			    print int(random_match)
 			    print "Found a match for: %s" % each_user[1]
 		            user_list.remove(random_match)
 			    break
